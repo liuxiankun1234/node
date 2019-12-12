c25410fbc95b3910c7376de6a38e8f88cc2d8be8
liuxiankun <18246059391@163.com>
2019-12-12 09:07:43 +0800

    修改

--- js/buffer.js
+++ js/buffer.js
@@ -6,7 +6,16 @@
  *      JS自有的字符串远远不能满足这些需求 于是Buffer应运而生
  * 
  *  Buffer结构 
- *      用于操作字节
+ *      一个像Array的对象 用于操作字节
+ * 
+ *      模块结构
+ *          Buffer是一个典型的JS和C++结合的模块 性能相关由C++实现 非性能相关由JS实现
+ *          Buffer所占用的内存不是V8分配的 属于堆外内存 由于V8垃圾回收性能的影响 将常用的操作对象用更高效和专有的内存分配回收策略来管理是个不错的思路
+ *          Buffer挂在到全局变量上 无需require('buffer')        
+ * 
+ *      Buffer对象
+ *          类似数组 元素为16进制的两位数 即0~255的数值
+ *          
  * 
  *  注意：
  *      Buffer类在全局作用域中 因此无需使用require('buffer')
@@ -19,7 +28,6 @@ const buffer2 = Buffer.from([1,2,3,4])
 
 const buffer3 = Buffer.alloc(20)
 
-console.log(buffer1)
+console.log(buffer1.length)
 console.log(buffer2)
 console.log(buffer3)
-console.log(global)
